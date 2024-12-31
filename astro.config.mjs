3dd36ab2dd663c2720d2b51ec6297853f3663d68
9node <rayyan@theninthnode.com>
2024-12-31 16:21:32 +0530

    added netlify build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,21 @@ import { defineConfig } from "astro/config";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
-import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic(), tailwind(), alpinejs()],
-	output: "server",
-	// prefetch: {
-	// 	prefetchAll: true,
-	// },
-	adapter: vercel(),
+  integrations: [react(), markdoc(), keystatic(), tailwind(), alpinejs()],
+
+  // prefetch: {
+  // 	prefetchAll: true,
+  // },
+  output: "server",
+
+  adapter: netlify(),
 });
